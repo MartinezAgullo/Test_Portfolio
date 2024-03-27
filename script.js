3bb74d2c540e460300c42cf3af32a8993b8cebf8
MartinezAgullo <pablo.martinez.agullo@cern.ch>
2024-03-27 13:58:13 +0100

    Add first set of modificitaions for personalisation

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ function addRecommendation() {
   // If the user has left a recommendation, display a pop-up
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
-    //Call showPopup here
-
-    // Create a new 'recommendation' element and set it's value to the user's message
+    // Create a new 'recommendation' element and set its value to the user's message
     var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    element.setAttribute("class", "recommendation");
+    element.innerHTML = "<span>&#8220;</span>" + recommendation.value + "<span>&#8221;</span>";
     // Add this element to the end of the list of recommendations
     document.getElementById("all_recommendations").appendChild(element); 
-    
+
+    // Call showPopup here with the confirmation message
+    showPopup(true, "Thank you for your recommendation! It has been added.");
+
     // Reset the value of the textarea
     recommendation.value = "";
   }
 }
 
-function showPopup(bool) {
+function showPopup(bool, message) {
+  var popup = document.getElementById('popup');
   if (bool) {
-    document.getElementById('popup').style.visibility = 'visible'
+    // Update the inner HTML of the popup's message container
+    popup.getElementsByTagName('h3')[0].innerHTML = message;
+    popup.style.visibility = 'visible';
   } else {
-    document.getElementById('popup').style.visibility = 'hidden'
+    popup.style.visibility = 'hidden';
   }
 }
